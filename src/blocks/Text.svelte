aece2e07ad55fc2c4edcb52592498d35826da4d9
Scott Tolinski <scott.tolinski@gmail.com>
2020-05-08 16:30:48 -0600

    adds rel="noopener"

--- src/blocks/Text.svelte
+++ src/blocks/Text.svelte
@@ -6,7 +6,9 @@
     <p>
         {#each block.properties.title as items}
             {#if items.length > 1}
-                <a href={items[1][0][1]}>{items[0]}</a>
+                <a href={items[1][0][1]} target="_blank" rel="noopener">
+                    {items[0]}
+                </a>
             {:else}{items[0]}{/if}
         {/each}
     </p>
